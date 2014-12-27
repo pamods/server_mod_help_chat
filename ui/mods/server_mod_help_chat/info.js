830f70869d5ab253c7bd18da7079e630340a71d4
Justin Love <git@JustinLove.name>
2014-12-27 09:57:40 -0600

    extract file loading to module

---
+++ ui/mods/server_mod_help_chat/info.js
@@ -0,0 +1,67 @@
+define(function() {
+  var help = ko.observable({})
+  var topics = ko.computed(function() {
+    return Object.keys(help())
+  })
+  var addTopics = function(t) {
+    help(_.extend(help(), t))
+  }
+  var specs = ko.observable({})
+  var addSpec = function(identifier, s) {
+    specs()[identifier] = s
+    specs(specs())
+  }
+
+  var record = function(identifier) {
+    return function(data) {
+      //console.log(data)
+      try {
+        var ext = JSON.parse(data)
+        if (ext) {
+          addSpec(identifier, ext)
+          if (ext.topics) {
+            addTopics(ext.topics)
+          } else {
+            console.log('no help topics found in', this.url)
+          }
+        } else {
+          console.log('did not parse', this.url)
+        }
+      } catch(e) {
+        console.log('json parsing error in file', this.url, e.message)
+      }
+    }
+  }
+  var failure = function() {
+    //console.log('no help found for', this.url)
+  }
+  var loadModHelp = function(mod) {
+    var url = 'coui://'+mod.identifier+'/server_mod_help.json'
+    $.ajax({
+      url: url,
+      success: record(mod.identifier),
+      error: failure
+    });
+  }
+
+  var mounted = function(mods) {
+    mods.forEach(loadModHelp)
+  }
+
+  api.mods.getMountedMods('client', mounted)
+  api.mods.getMountedMods('server', mounted)
+
+  if (handlers.mount_mod_file_data) {
+    var base_mount_mod_file_data = handlers.mount_mod_file_data
+    handlers.mount_mod_file_data = function() {
+      base_mount_mod_file_data.apply(this, arguments)
+      api.mods.getMountedMods('server', mounted)
+    }
+  }
+
+  return {
+    specs: specs,
+    help: help,
+    topics: topics,
+  }
+})
--- ui/mods/server_mod_help_chat/new_game.js
+++ ui/mods/server_mod_help_chat/new_game.js
@@ -1,50 +1,14 @@
+console.log('help chat');
+
 (function() {
-  console.log('help chat')
-
-  var help = ko.observable({})
-  var topics = ko.computed(function() {
-    return Object.keys(help())
-  })
-  var addTopics = function(t) {
-    help(_.extend(help(), t))
-    console.log(help())
-  }
-
-  var success = function(data) {
-    //console.log(data)
-    try {
-      var ext = JSON.parse(data)
-      if (ext && ext.topics) {
-        addTopics(ext.topics)
-      } else {
-        console.log('no help topics found in', this.url)
-      }
-    } catch(e) {
-      console.log('json parsing error in file', this.url, e.message)
-    }
-  }
-  var failure = function() {
-    //console.log('no help found for', this.url)
-  }
-  var loadModHelp = function(mod) {
-    var url = 'coui://'+mod.identifier+'/server_mod_help.json'
-    $.ajax({
-      url: url,
-      success: success,
-      error: failure
-    });
-  }
-
-  var mounted = function(mods) {
-    mods.forEach(loadModHelp)
-  }
-
-  api.mods.getMountedMods('client', mounted)
-  api.mods.getMountedMods('server', mounted)
-
-  var base_mount_mod_file_data = handlers.mount_mod_file_data
-  handlers.mount_mod_file_data = function() {
-    base_mount_mod_file_data.apply(this, arguments)
-    api.mods.getMountedMods('server', mounted)
-  }
+  var config = require.s.contexts._.config
+  config.waitSeconds = 0
+  config.paths.server_mod_help_chat = 'coui://ui/mods/server_mod_help_chat'
 })()
+
+require(['server_mod_help_chat/info'], function(info) {
+  var print = function(data) {console.log(data)}
+  info.specs.subscribe(print)
+  info.help.subscribe(print)
+  info.topics.subscribe(print)
+})
