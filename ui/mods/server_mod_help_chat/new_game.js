b924e33a0203f0f8a31ea160b690ed6e2e513adb
Justin Love <git@JustinLove.name>
2014-12-27 09:36:16 -0600

    cataloging topics

--- ui/mods/server_mod_help_chat/new_game.js
+++ ui/mods/server_mod_help_chat/new_game.js
@@ -1,10 +1,24 @@
 (function() {
   console.log('help chat')
 
+  var help = ko.observable({})
+  var topics = ko.computed(function() {
+    return Object.keys(help())
+  })
+  var addTopics = function(t) {
+    help(_.extend(help(), t))
+    console.log(help())
+  }
+
   var success = function(data) {
     //console.log(data)
     try {
-      console.log(JSON.parse(data))
+      var ext = JSON.parse(data)
+      if (ext && ext.topics) {
+        addTopics(ext.topics)
+      } else {
+        console.log('no help topics found in', this.url)
+      }
     } catch(e) {
       console.log('json parsing error in file', this.url, e.message)
     }
@@ -28,4 +42,9 @@
   api.mods.getMountedMods('client', mounted)
   api.mods.getMountedMods('server', mounted)
 
+  var base_mount_mod_file_data = handlers.mount_mod_file_data
+  handlers.mount_mod_file_data = function() {
+    base_mount_mod_file_data.apply(this, arguments)
+    api.mods.getMountedMods('server', mounted)
+  }
 })()
